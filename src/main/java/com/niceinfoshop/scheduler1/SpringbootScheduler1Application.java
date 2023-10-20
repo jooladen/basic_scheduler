a04eb09845d8f2957515f9870bb62738aa648a52
jooladen <jooladen@gmail.com>
2023-10-20 23:35:48 +0900

    first

---
+++ src/main/java/com/niceinfoshop/scheduler1/SpringbootScheduler1Application.java
@@ -0,0 +1,13 @@
+package com.niceinfoshop.scheduler1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootScheduler1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootScheduler1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/niceinfoshop/scheduler1/SpringbootScheduler1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niceinfoshop.scheduler1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootScheduler1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
